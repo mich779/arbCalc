bbbde66d14cfc9fdeefff53fecec349e172c8679
michael <borinskym@gmail.com>
2018-01-31 11:57:42 +0200

    add binance api to the project

---
+++ src/main/java/com/binance/api/client/BinanceApiWebSocketClient.java
@@ -0,0 +1,25 @@
+package com.binance.api.client;
+
+import java.util.List;
+import com.binance.api.client.domain.event.AggTradeEvent;
+import com.binance.api.client.domain.event.AllMarketTickersEvent;
+import com.binance.api.client.domain.event.CandlestickEvent;
+import com.binance.api.client.domain.event.DepthEvent;
+import com.binance.api.client.domain.event.UserDataUpdateEvent;
+import com.binance.api.client.domain.market.CandlestickInterval;
+
+/**
+ * Binance API data streaming façade, supporting streaming of events through web sockets.
+ */
+public interface BinanceApiWebSocketClient {
+
+  void onDepthEvent(String symbol, BinanceApiCallback<DepthEvent> callback);
+
+  void onCandlestickEvent(String symbol, CandlestickInterval interval, BinanceApiCallback<CandlestickEvent> callback);
+
+  void onAggTradeEvent(String symbol, BinanceApiCallback<AggTradeEvent> callback);
+
+  void onUserDataUpdateEvent(String listenKey, BinanceApiCallback<UserDataUpdateEvent> callback);
+
+  void onAllMarketTickersEvent(BinanceApiCallback<List<AllMarketTickersEvent>> callback);
+}
