7edb15f3c4c568f85c842014d2dff338dfc8aa69
michael <borinskym@gmail.com>
2018-02-06 21:22:32 +0200

    equals and hash code

--- src/main/java/com/romanobori/MyArbOrder.java
+++ src/main/java/com/romanobori/MyArbOrder.java
@@ -1,6 +1,7 @@
 package com.romanobori;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MyArbOrder {
 
@@ -20,5 +21,26 @@ public class MyArbOrder {
         this.executedQuantity = executedQuantity;
         this.action = action;
         this.time = time;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyArbOrder that = (MyArbOrder) o;
+        return Double.compare(that.price, price) == 0 &&
+                Double.compare(that.origQuantity, origQuantity) == 0 &&
+                Double.compare(that.executedQuantity, executedQuantity) == 0 &&
+                time == that.time &&
+                Objects.equals(symbol, that.symbol) &&
+                Objects.equals(orderId, that.orderId) &&
+                action == that.action;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(symbol, orderId, price, origQuantity, executedQuantity, action, time);
     }
 }
