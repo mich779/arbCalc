3b345c2fe55e60eabd2b93e18330979010e31c45
Romano <amitromano3@gmail.com>
2018-02-09 16:20:03 +0200

    implemented binanceApiClient

--- src/main/java/com/romanobori/ApiClient.java
+++ src/main/java/com/romanobori/ApiClient.java
@@ -7,9 +7,10 @@ public interface ApiClient {
     public ArbOrders getOrderBook(String symbol);
     public List<MyArbOrder> getMyOrders();
     public void addArbOrder(NewArbOrder order);
-    public void cancelOrder(long orderId);
+    public void cancelOrder(MyArbOrder order);
     public void cancelAllOrders();
-    public void withdrawal(long withrawalId);
+    public void withdrawal(ArbWalletEntry withdrawalDetails);
+
     public ArbWallet getWallet();
 
 }
